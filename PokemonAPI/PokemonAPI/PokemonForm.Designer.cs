349fc1e8310675466154ca2f85529297795fcb51
Jackson Monson <84549320+JacquesCartier2@users.noreply.github.com>
2023-04-24 09:38:25 -0400

    Add files via upload

---
+++ PokemonAPI/PokemonAPI/PokemonForm.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace PokemonAPI
+{
+    partial class PokemonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txb_Pokemon = new System.Windows.Forms.TextBox();
+            this.lbl_Input = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Happiness = new System.Windows.Forms.Label();
+            this.lbl_Capture = new System.Windows.Forms.Label();
+            this.lbl_Habitat = new System.Windows.Forms.Label();
+            this.lbl_Growth = new System.Windows.Forms.Label();
+            this.lbl_Flavor = new System.Windows.Forms.Label();
+            this.lbl_Egg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.Label();
+            this.txt_Happiness = new System.Windows.Forms.Label();
+            this.txt_Capture = new System.Windows.Forms.Label();
+            this.txt_Habitat = new System.Windows.Forms.Label();
+            this.txt_Growth = new System.Windows.Forms.Label();
+            this.txt_Flavor = new System.Windows.Forms.Label();
+            this.txt_Egg = new System.Windows.Forms.Label();
+            this.btn_Pokemon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txb_Pokemon
+            // 
+            this.txb_Pokemon.Location = new System.Drawing.Point(12, 49);
+            this.txb_Pokemon.Name = "txb_Pokemon";
+            this.txb_Pokemon.Size = new System.Drawing.Size(190, 20);
+            this.txb_Pokemon.TabIndex = 0;
+            // 
+            // lbl_Input
+            // 
+            this.lbl_Input.AutoSize = true;
+            this.lbl_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Input.Location = new System.Drawing.Point(9, 20);
+            this.lbl_Input.Name = "lbl_Input";
+            this.lbl_Input.Size = new System.Drawing.Size(193, 17);
+            this.lbl_Input.TabIndex = 1;
+            this.lbl_Input.Text = "Enter a pokemon name or ID:";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Name.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Name.Location = new System.Drawing.Point(153, 90);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(49, 17);
+            this.lbl_Name.TabIndex = 2;
+            this.lbl_Name.Text = "Name:";
+            this.lbl_Name.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Happiness
+            // 
+            this.lbl_Happiness.AutoSize = true;
+            this.lbl_Happiness.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Happiness.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Happiness.Location = new System.Drawing.Point(87, 123);
+            this.lbl_Happiness.Name = "lbl_Happiness";
+            this.lbl_Happiness.Size = new System.Drawing.Size(115, 17);
+            this.lbl_Happiness.TabIndex = 3;
+            this.lbl_Happiness.Text = "Base Happiness:";
+            this.lbl_Happiness.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Capture
+            // 
+            this.lbl_Capture.AutoSize = true;
+            this.lbl_Capture.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Capture.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Capture.Location = new System.Drawing.Point(106, 156);
+            this.lbl_Capture.Name = "lbl_Capture";
+            this.lbl_Capture.Size = new System.Drawing.Size(96, 17);
+            this.lbl_Capture.TabIndex = 4;
+            this.lbl_Capture.Text = "Capture Rate:";
+            this.lbl_Capture.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Habitat
+            // 
+            this.lbl_Habitat.AutoSize = true;
+            this.lbl_Habitat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Habitat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Habitat.Location = new System.Drawing.Point(145, 188);
+            this.lbl_Habitat.Name = "lbl_Habitat";
+            this.lbl_Habitat.Size = new System.Drawing.Size(57, 17);
+            this.lbl_Habitat.TabIndex = 5;
+            this.lbl_Habitat.Text = "Habitat:";
+            this.lbl_Habitat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Growth
+            // 
+            this.lbl_Growth.AutoSize = true;
+            this.lbl_Growth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Growth.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Growth.Location = new System.Drawing.Point(111, 221);
+            this.lbl_Growth.Name = "lbl_Growth";
+            this.lbl_Growth.Size = new System.Drawing.Size(91, 17);
+            this.lbl_Growth.TabIndex = 6;
+            this.lbl_Growth.Text = "Growth Rate:";
+            this.lbl_Growth.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Flavor
+            // 
+            this.lbl_Flavor.AutoSize = true;
+            this.lbl_Flavor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Flavor.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Flavor.Location = new System.Drawing.Point(119, 292);
+            this.lbl_Flavor.Name = "lbl_Flavor";
+            this.lbl_Flavor.Size = new System.Drawing.Size(82, 17);
+            this.lbl_Flavor.TabIndex = 7;
+            this.lbl_Flavor.Text = "Flavor Text:";
+            this.lbl_Flavor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Egg
+            // 
+            this.lbl_Egg.AutoSize = true;
+            this.lbl_Egg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_Egg.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_Egg.Location = new System.Drawing.Point(120, 257);
+            this.lbl_Egg.Name = "lbl_Egg";
+            this.lbl_Egg.Size = new System.Drawing.Size(81, 17);
+            this.lbl_Egg.TabIndex = 8;
+            this.lbl_Egg.Text = "Egg Group:";
+            this.lbl_Egg.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(376, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 9;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.AutoSize = true;
+            this.txt_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Name.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Name.Location = new System.Drawing.Point(218, 90);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(13, 17);
+            this.txt_Name.TabIndex = 10;
+            this.txt_Name.Text = "*";
+            this.txt_Name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Happiness
+            // 
+            this.txt_Happiness.AutoSize = true;
+            this.txt_Happiness.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Happiness.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Happiness.Location = new System.Drawing.Point(218, 123);
+            this.txt_Happiness.Name = "txt_Happiness";
+            this.txt_Happiness.Size = new System.Drawing.Size(13, 17);
+            this.txt_Happiness.TabIndex = 11;
+            this.txt_Happiness.Text = "*";
+            this.txt_Happiness.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Capture
+            // 
+            this.txt_Capture.AutoSize = true;
+            this.txt_Capture.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Capture.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Capture.Location = new System.Drawing.Point(218, 156);
+            this.txt_Capture.Name = "txt_Capture";
+            this.txt_Capture.Size = new System.Drawing.Size(13, 17);
+            this.txt_Capture.TabIndex = 12;
+            this.txt_Capture.Text = "*";
+            this.txt_Capture.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Habitat
+            // 
+            this.txt_Habitat.AutoSize = true;
+            this.txt_Habitat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Habitat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Habitat.Location = new System.Drawing.Point(218, 188);
+            this.txt_Habitat.Name = "txt_Habitat";
+            this.txt_Habitat.Size = new System.Drawing.Size(13, 17);
+            this.txt_Habitat.TabIndex = 13;
+            this.txt_Habitat.Text = "*";
+            this.txt_Habitat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Growth
+            // 
+            this.txt_Growth.AutoSize = true;
+            this.txt_Growth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Growth.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Growth.Location = new System.Drawing.Point(218, 221);
+            this.txt_Growth.Name = "txt_Growth";
+            this.txt_Growth.Size = new System.Drawing.Size(13, 17);
+            this.txt_Growth.TabIndex = 14;
+            this.txt_Growth.Text = "*";
+            this.txt_Growth.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Flavor
+            // 
+            this.txt_Flavor.AutoSize = true;
+            this.txt_Flavor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Flavor.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Flavor.Location = new System.Drawing.Point(218, 292);
+            this.txt_Flavor.Name = "txt_Flavor";
+            this.txt_Flavor.Size = new System.Drawing.Size(13, 17);
+            this.txt_Flavor.TabIndex = 15;
+            this.txt_Flavor.Text = "*";
+            this.txt_Flavor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txt_Egg
+            // 
+            this.txt_Egg.AutoSize = true;
+            this.txt_Egg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt_Egg.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txt_Egg.Location = new System.Drawing.Point(218, 257);
+            this.txt_Egg.Name = "txt_Egg";
+            this.txt_Egg.Size = new System.Drawing.Size(13, 17);
+            this.txt_Egg.TabIndex = 16;
+            this.txt_Egg.Text = "*";
+            this.txt_Egg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_Pokemon
+            // 
+            this.btn_Pokemon.Location = new System.Drawing.Point(221, 47);
+            this.btn_Pokemon.Name = "btn_Pokemon";
+            this.btn_Pokemon.Size = new System.Drawing.Size(75, 23);
+            this.btn_Pokemon.TabIndex = 17;
+            this.btn_Pokemon.Text = "Submit";
+            this.btn_Pokemon.UseVisualStyleBackColor = true;
+            this.btn_Pokemon.Click += new System.EventHandler(this.btn_Pokemon_Click);
+            // 
+            // PokemonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Pokemon);
+            this.Controls.Add(this.txt_Egg);
+            this.Controls.Add(this.txt_Flavor);
+            this.Controls.Add(this.txt_Growth);
+            this.Controls.Add(this.txt_Habitat);
+            this.Controls.Add(this.txt_Capture);
+            this.Controls.Add(this.txt_Happiness);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Egg);
+            this.Controls.Add(this.lbl_Flavor);
+            this.Controls.Add(this.lbl_Growth);
+            this.Controls.Add(this.lbl_Habitat);
+            this.Controls.Add(this.lbl_Capture);
+            this.Controls.Add(this.lbl_Happiness);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_Input);
+            this.Controls.Add(this.txb_Pokemon);
+            this.Name = "PokemonForm";
+            this.Text = "Pokemon";
+            this.Load += new System.EventHandler(this.Pokemon_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txb_Pokemon;
+        private System.Windows.Forms.Label lbl_Input;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Happiness;
+        private System.Windows.Forms.Label lbl_Capture;
+        private System.Windows.Forms.Label lbl_Habitat;
+        private System.Windows.Forms.Label lbl_Growth;
+        private System.Windows.Forms.Label lbl_Flavor;
+        private System.Windows.Forms.Label lbl_Egg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txt_Name;
+        private System.Windows.Forms.Label txt_Happiness;
+        private System.Windows.Forms.Label txt_Capture;
+        private System.Windows.Forms.Label txt_Habitat;
+        private System.Windows.Forms.Label txt_Growth;
+        private System.Windows.Forms.Label txt_Flavor;
+        private System.Windows.Forms.Label txt_Egg;
+        private System.Windows.Forms.Button btn_Pokemon;
+    }
+}
---
+++ PokemonAPI/PokemonAPI/PokemonForm.cs
@@ -0,0 +1,151 @@
+﻿using PokeAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonAPI
+{
+    public partial class PokemonForm : Form
+    {
+        PokemonSpecies species;
+        string defaultText = "*";
+
+        public PokemonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Pokemon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //get a pokemon from the API based on user input. Users may input a name or ID. 
+        private async void btn_Pokemon_Click(object sender, EventArgs e)
+        {
+            string input = txb_Pokemon.Text;
+
+            await GetSpeciesAsync(input);
+
+            ShowSpeciesInfo();
+        }
+
+        //return a pokemon based on name or ID.
+        async Task GetSpeciesAsync(string _input)
+        {
+            bool usingID = false;
+            int ID = 0;
+
+            //if the input can be parsed use it as an ID.
+            try
+            {
+                ID = int.Parse(_input);
+                usingID = true;
+            }
+            catch
+            {
+
+            }
+
+            //set the species variable using the API and either ID or _input.
+            if (usingID)
+            {
+                try
+                {
+                    species = await DataFetcher.GetApiObject<PokemonSpecies>(ID);
+                }
+                catch
+                {
+                    MessageBox.Show(ID + " is not a valid ID.");
+                    return;
+                }
+                
+            }
+            else
+            {
+                try
+                {
+                    species = await DataFetcher.GetNamedApiObject<PokemonSpecies>(_input);
+                }
+                catch
+                {
+                    MessageBox.Show(_input + " is not a valid name.");
+                    return;
+                }
+            }
+        }
+
+        //show the information of the species on the form. Any unavailable fields will show the default text.
+        void ShowSpeciesInfo()
+        {
+            try
+            {
+                txt_Name.Text = species.Name;
+            }
+            catch
+            {
+                txt_Name.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Happiness.Text = species.BaseHappiness.ToString();
+            }
+            catch
+            {
+                txt_Happiness.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Capture.Text = species.CaptureRate.ToString();
+            }
+            catch
+            {
+                txt_Capture.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Habitat.Text = species.Habitat.Name;
+            }
+            catch
+            {
+                txt_Habitat.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Growth.Text = species.GrowthRate.Name;
+            }
+            catch
+            {
+                txt_Growth.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Flavor.Text = species.FlavorTexts[0].FlavorText;
+            }
+            catch
+            {
+                txt_Flavor.Text = defaultText;
+            }
+
+            try
+            {
+                txt_Egg.Text = species.EggGroups[0].Name; ;
+            }
+            catch
+            {
+                txt_Egg.Text = defaultText;
+            }
+        }
+    }
+}
